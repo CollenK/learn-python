700041dfe04660127ea9b54f1de9dcf1181aa5f1
CollenK <collen.kriel@gmail.com>
2016-10-19 12:49:16 +0700

    Complete OOP in Python

--- movie-system/movie.py
+++ movie-system/movie.py
@@ -6,3 +6,15 @@ class Movie:
 
     def __repr__(self):
         return "<Movie {}>".format(self.name)
+
+    def json(self):
+        return {
+            'name': self.name,
+            'genre': self.genre,
+            'watched': self.watched
+        }
+
+    @classmethod
+    def from_json(cls, json_data):
+        # return Movie(json_data['name'], json_data['genre'], json_data['watched'])
+        return Movie(**json_data) # Create set of named parameters (argument unpacking)
